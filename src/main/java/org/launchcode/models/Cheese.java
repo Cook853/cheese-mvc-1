ae84fe3ccbe9c523dc8de7705e94d566949884b2
Andrew Cook <andrew.scott.cook.law@gmail.com>
2017-04-10 22:55:33 -0500

    Hopefully replaced with persistent starter code...

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -2,9 +2,6 @@ package org.launchcode.models;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.persistence.Entity;
@@ -20,31 +17,24 @@ public class Cheese {
     private int id;
 
     @NotNull
-    @Size(min=3, max=15, message = "Name must be between 3 and 15 characters")
+    @Size(min=3, max=15)
     private String name;
 
     @NotNull
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    @ManyToOne
-    private Category category;
+    private CheeseType type;
 
-    @NotNull
-    @Max(5)
-    @Min(1)
-    private Integer rating;
-
-    public Cheese() {
-
-    }
-
-    public Cheese(String name, String description,
-                  Category category, Integer rating) {
+    public Cheese(String name, String description) {
         this.name = name;
         this.description = description;
-        this.category = category;
-        this.rating = rating;
+    }
+
+    public Cheese() { }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -63,21 +53,11 @@ public class Cheese {
         this.description = description;
     }
 
-    public Integer getRating() {
-        return rating;
-    }
-    public void setRating(Integer rating) {
-        this.rating = rating;
+    public CheeseType getType() {
+        return type;
     }
 
-    public Category getCategory() {
-        return category;
-    }
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public int getId() {
-        return id;
+    public void setType(CheeseType type) {
+        this.type = type;
     }
 }
